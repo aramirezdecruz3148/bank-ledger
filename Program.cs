ef8310bad09ff90b28988a403300ac4db77138e7
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-27 12:02:50 -0700

    created a User class with methods for gathering user information as well as greeting to the user, created a new instance of class and printed greeting to console to ensure it is working

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 
-namespace bank_ledger
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace bank_ledger {
+    public class User {
+        public string Username { get; private set; }
+        public string Nickname { get; private set; }
+        public int PinNumber { get; private set; }
+        // public User() { }
+        public User(string username, string nickname, int pinNumber) {
+            Username = username;
+            Nickname = nickname;
+            PinNumber = pinNumber;
+        }
+        public void GreetingUser() {
+            Console.WriteLine("Welcome to deCruz Bank, {0}!", Nickname);
+        }
+        
+    }
+    class Program {
+        static void Main(string[] args) {
+        User test = new User("aramirez", "alex", 1234);
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Adding a line");
+            test.GreetingUser();
         }
     }
 }
