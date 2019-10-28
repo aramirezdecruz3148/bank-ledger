fdbcaf5d13909364e955fd9933fdaf4457f5468e
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-28 14:20:49 -0700

    added a method to create a user

--- Program.cs
+++ Program.cs
@@ -5,10 +5,20 @@ namespace bank_ledger {
         public string Username { get; private set; }
         public string Nickname { get; private set; }
         public int PinNumber { get; private set; }
-        public double InitialDeposit { get; private set; }
-        
+        public decimal InitialBalance { get; private set; }
+        public void CreateUser() {
+            Console.WriteLine("To begin banking please create an account...");
+            Console.WriteLine("Enter a username: ");
+            Username = Console.ReadLine();
+            Console.WriteLine("Enter a 4 digit pin number: ");
+            PinNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your initial deposit amount for your new account: ");
+            InitialBalance = Decimal.Parse(Console.ReadLine());
+            Console.Clear();
+        }
     }
     class Program {
         static void Main(string[] args) {
+        }
     }
 }
