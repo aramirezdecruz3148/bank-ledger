5502d50a6da8ee32723a183caa44b7de8d567188
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-28 08:35:05 -0700

    added an initial deposit amount when the user first creates their account

--- Program.cs
+++ Program.cs
@@ -5,11 +5,12 @@ namespace bank_ledger {
         public string Username { get; private set; }
         public string Nickname { get; private set; }
         public int PinNumber { get; private set; }
-        // public User() { }
-        public User(string username, string nickname, int pinNumber) {
+        public double InitialDeposit { get; private set; }
+        public User(string username, string nickname, int pinNumber, double initialDeposit) {
             Username = username;
             Nickname = nickname;
             PinNumber = pinNumber;
+            InitialDeposit = initialDeposit;
         }
         public void GreetingUser() {
             Console.WriteLine("Welcome to deCruz Bank, {0}!", Nickname);
@@ -18,7 +19,7 @@ namespace bank_ledger {
     }
     class Program {
         static void Main(string[] args) {
-        User test = new User("aramirez", "alex", 1234);
+        User test = new User("aramirez", "alex", 1234, 500.00);
             Console.WriteLine("Hello World!");
             test.GreetingUser();
         }
