48080e82438f7b5c4bfb17609b05bcb487ed5c91
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-28 14:17:19 -0700

    got a little ahead of myself with how I was structuring data for file writing, would like to mock out an MVP version of functionality first

--- Program.cs
+++ Program.cs
@@ -6,22 +6,9 @@ namespace bank_ledger {
         public string Nickname { get; private set; }
         public int PinNumber { get; private set; }
         public double InitialDeposit { get; private set; }
-        public User(string username, string nickname, int pinNumber, double initialDeposit) {
-            Username = username;
-            Nickname = nickname;
-            PinNumber = pinNumber;
-            InitialDeposit = initialDeposit;
-        }
-        public void GreetingUser() {
-            Console.WriteLine("Welcome to deCruz Bank, {0}!", Nickname);
-        }
         
     }
     class Program {
         static void Main(string[] args) {
-        User test = new User("aramirez", "alex", 1234, 500.00);
-            Console.WriteLine("Hello World!");
-            test.GreetingUser();
-        }
     }
 }
